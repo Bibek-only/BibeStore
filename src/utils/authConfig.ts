b6b629de3c6750bef0dcd05684a0800ff99de7bb
Bibek-only <bibekbibek966@gmail.com>
2025-05-31 00:43:28 +0530

    email verification setup with proper mail sendig feture with nodemailer

--- src/utils/authConfig.ts
+++ src/utils/authConfig.ts
@@ -1,6 +1,8 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { varible } from "@/schemas/envSchema";
+import prisma from "@/db/db";
+import bcrypt from "bcryptjs";
 
 export const authConfig = {
   providers: [
@@ -16,6 +18,7 @@ export const authConfig = {
         },
       },
     }),
+
     CredentialsProvider({
       name: "Credentials",
       credentials: {
@@ -27,28 +30,73 @@ export const authConfig = {
         },
       },
       async authorize(credentials: any, req: any) {
-        //perform authentication logic here
+        //check email is presen in db or not
+        const userRes = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (userRes) {
+          //compare the password is corect or not
+          const passRes = await bcrypt.compare(
+            credentials.password,
+            userRes.password as string
+          );
+          if (passRes) {
+            return {
+              id: userRes.id,
+              userId: userRes.id,
+              email: userRes.email,
+              name: userRes.name,
+            };
+          }
+        }
 
-        //    return if user is authenticated and if not then return null
-        return {
-          id: "user-1",
-          email: credentials?.email,
-        };
-        // return null;
+        return null;
       },
     }),
   ],
   secret: varible.NEXTAUTH_SECRET,
   callbacks: {
+    async jwt({ token, user, account, profile, isNewUser }: any) {
+      if (account?.provider === "google") {
+        try {
+          //check is email is exist or not
+          const emailRes = await prisma.user.findUnique({
+            where: {
+              email: token.email,
+            },
+          });
+
+          // create the user if the user is not exist
+          if (!emailRes) {
+            const userRes = await prisma.user.create({
+              data: {
+                email: token.email as string,
+                name: token.name as string,
+                profileImageUrl: token.picture as string,
+                password: "google",
+                isEmailVerifyed: true,
+              },
+            });
+            //create token and return
+            token.userId = userRes.id;
+            return token;
+          } else {
+            token.userId = emailRes.id;
+            return token;
+          }
+        } catch (error) {
+          console.log("error in catch of user db operation", error);
+        }
+      }
+      token.userId = token.sub;
+      return token;
+    },
     async session({ session, user, token }: any) {
+      session.user.userId = token.sub;
 
       return session;
-
-    },
-    async jwt({ token, user, account, profile, isNewUser }: any) {
-     
-      return token;
-      
     },
   },
 };
